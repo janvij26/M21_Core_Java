69a218ae26dcbf9a212ddfc322dcbb441a7b3ee1
USER <USER@Janvi>
2022-07-13 01:03:53 +0530

    First Commit

---
+++ Day2_Intro_To_Java/src/com/cg/java/Program1.java
@@ -0,0 +1,17 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args)
+	{
+		//double a=15.0f;
+		Scanner s=new Scanner(System.in);
+		double a=s.nextDouble();
+		System.out.println("The Value of a is : "+a);
+		//System.out.println("Welcome To C2TC");
+	}
+	
+
+}
---
+++ Day2_Intro_To_Java/src/com/cg/java/Program2.java
@@ -0,0 +1,15 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		String str1=s.next();
+		System.out.println("The String is : "+str+" "+str1);
+
+	}
+
+}
