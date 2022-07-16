12ba686bc5ae839e593b7b9a566349c683314d01
USER <USER@Janvi>
2022-07-17 01:36:57 +0530

    Second Commit

--- Assignment-1/src/com/tns/operator/RelationalOperator.java
+++ Assignment-1/src/com/tns/operator/RelationalOperator.java
@@ -9,8 +9,25 @@ public class RelationalOperator {
 	  Scanner s=new Scanner(System.in);
 	  int a=s.nextInt();
 	  int b=s.nextInt();
-	  boolean res=(a==b);
-	  System.out.println(res);
+	  
+	  boolean result_1=a>b; 
+	  System.out.println(result_1);
+	  
+	  boolean result_2=a<b;
+	  System.out.println(result_2);
+	  
+	  boolean result_3=a>=b;
+	  System.out.println(result_3);
+	  
+	  boolean result_4=a<=b;
+	  System.out.println(result_4);
+	  
+	  boolean result_5=a!=b;
+	  System.out.println(result_5);
+	  
+	  boolean result_6=(a==b);
+	  
+	  System.out.println(result_6);
 	  s.close();
 	}
 
