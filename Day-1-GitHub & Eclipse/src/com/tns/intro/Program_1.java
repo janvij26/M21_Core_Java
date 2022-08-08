882ff47b4799fe126562c6b161f8afe109f6382b
USER <USER@Janvi>
2022-08-08 20:15:50 +0530

    First Commit

---
+++ Day-1-GitHub & Eclipse/src/com/tns/intro/Program_1.java
@@ -0,0 +1,12 @@
+package com.tns.intro;
+
+public class Program_1 {
+
+	public static void main(String[] args) {
+		int a=5;
+		System.out.print("The Value of a is :");  
+		System.out.print(a);
+
+	}
+
+}
---
+++ Day-1-GitHub & Eclipse/src/com/tns/intro1/Program_2.java
@@ -0,0 +1,11 @@
+package com.tns.intro1;
+
+public class Program_2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hey there, I am using Eclipse");
+
+	}
+
+}
